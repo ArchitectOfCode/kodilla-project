029ef953ad7db8d95923bc65269aa10922a05ec4
ArchitectOfCode <zbigniew_g@o2.pl>
2019-03-25 08:16:20 +0100

    Exercise 21.3: Tests for TaskController

--- src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
@@ -13,11 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-
-import org.springframework.test.web.servlet.ResultActions;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
